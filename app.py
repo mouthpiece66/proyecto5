3b8f5bd0469003f7a9b1d3dfbba462842e14f3c9
mouthpiece66 <nathalia.alvear@hotmail.com>
2024-08-05 16:03:42 +0100

    git add app.py notebooks/vehicles_us.csv streamlit/config.toml git status git commit -m "Add Streamlit configuration file and other changes" git push origin main

---
+++ app.py
@@ -0,0 +1,37 @@
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car data = pd.read_cvs('vehicles_us.csv')  # leer datos
+st.header('Análisis de Datos de Vehículos en Venta en EE.UU.')
+
+# Crear botón para construir un histograma
+hist_button = st.button('Construir histograma')
+
+# Al hacer clic en el botón del histograma
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    # Crear un histograma
+    fig_hist = px.histogram(car_data, x="odometer")
+    # Mostrar el gráfico de histograma
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Crear segundo botón para construir  gráfico de dispersión
+scatter_button = st.button('Construir gráfico de dispersión')
+
+# Al hacer clic en el botón del gráfico de dispersión
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    # Crear un gráfico de dispersión
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", kind="scatter")
+    # Mostrar el gráfico de dispersión
+    st.plotly_chart(fig_scatter, use_container_width=True)
+
+    # crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram:  # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+    ...
